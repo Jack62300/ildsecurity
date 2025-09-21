f89f688210c164bfa4041ee4a0b151b300689153
Jack Oneill <devunity62400@gmail.com>
2025-09-21 15:32:55 +0200

    Update controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -30,7 +30,7 @@ final class IndexController extends AbstractController
         $request->query->getInt('page', 1), // page courante
         12                                  // éléments par page
     );
-    dump($qb);
+    // dump($qb);
 
     return $this->render('index/index.html.twig', [
         'controller_name' => 'IndexController',
